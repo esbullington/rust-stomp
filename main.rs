8c31f1d1ccd2c52817528fbd86b32de7cf8998f8
Eric S. Bullington <eric.s.bullington@gmail.com>
2014-07-17 19:53:55 -0400

    Working client

--- main.rs
+++ main.rs
@@ -1,11 +1,117 @@
+#![feature(phase)]
 extern crate debug;
+#[phase(plugin, link)]  extern crate log;
 
 use std::io::TcpStream;
 use std::collections::HashMap;
 use std::fmt;
-use std::io::IoResult;
+use std::io::{IoResult, BufferedReader};
 
 
+#[deriving(PartialEq, Show)]
+pub enum ClientCommand {
+  STOMP,
+  CONNECT,
+  SUBSCRIBE,
+  UNSUBSCRIBE,
+  ACK,
+  NACK,
+  BEGIN,
+  COMMIT,
+  ABORT,
+  DISCONNECT,
+}
+
+#[deriving(PartialEq, Show)]
+pub enum ServerCommand {
+  CONNECTED,
+  MESSAGE,
+  RECEIPT,
+  ERROR,
+}
+
+#[allow(non_camel_case_types)]
+pub enum StompVersion {
+  STOMP_1_1,
+  STOMP_1_0,
+}
+
+impl fmt::Show for StompVersion {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match *self {
+      STOMP_1_1 => write!(f, "STOMP/1.1"),
+      STOMP_1_0 => write!(f, "STOMP/1.0"),
+    }
+  }
+}
+
+pub struct Response<'a> {
+  pub command: ServerCommand,
+  pub headers: HashMap<String, Vec<String>>,
+  stream: TcpStream,
+}
+
+impl<'a> Response<'a> {
+  pub fn with_stream(s: &TcpStream) -> Response {
+    let mut stream = BufferedReader::with_capacity(1, s.clone());
+
+    let command = match stream.read_line().unwrap().as_slice().trim() {
+      "CONNECTED"   => CONNECTED,
+      "MESSAGE"     => MESSAGE,
+      "RECEIPT"     => RECEIPT,
+      "ERROR"       => fail!("Server error"),
+      _             => fail!("Invalid STOMP command")
+    };
+
+    /*let version = match *segs.get(0) {*/
+    /*  "1.1"          => STOMP_1_1,*/
+    /*  _             => fail!("unsupported STOMP version")*/
+    /*};*/
+
+    println!("Got STOMP Response command = {:?}", command);
+
+    let mut headers = HashMap::new();
+    loop {
+      let line = stream.read_line().unwrap();
+      let segs = line.as_slice().splitn(':', 1).collect::<Vec<&str>>();
+      /*println!("Segs is: {}", segs);*/
+      if segs.len() == 2 {
+        let k = segs.get(0).trim();
+        let v = segs.get(1).trim();
+        headers.insert_or_update_with(k.to_string(), vec!(v.into_string()),
+                        |_k, ov| ov.push(v.into_string()));
+      } 
+      else {
+        if ["\n".to_string(), "\0".to_string()].contains(&line) {
+          break;
+        }
+        /*println!("Fail: {}", segs);*/
+        fail!("malformatted line");
+      }
+
+    }
+
+    Response { command: command, headers: headers, stream: stream.unwrap() }
+
+  }
+
+  fn parse_stream_to_string(&mut self) -> String {
+    let mut line = std::string::String::new();
+    loop {
+      match self.stream.read_byte() {
+        Ok(0x00)                    => {
+          break;
+        }
+        Ok(c)                       => {
+          line.push_char(c as char); 
+        }
+        Err(_)                      => {}
+      };
+    };
+    line
+  }
+
+}
 
 struct Frame {
   command: String,
@@ -19,18 +125,19 @@ impl Frame {
     Frame{ command: command, headers: HashMap::new(), body: String::new() }
   }
 
-  pub fn setHeader(&mut self, key: String, value: String) {
+  pub fn set_header(&mut self, key: String, value: String) {
     self.headers.insert(key, value);
   }
 
-  pub fn setBody(&mut self, text: String) {
+  pub fn set_body(&mut self, text: String) {
     self.body = text;
   }
 
-  pub fn writeRequest(&self, w: &mut Writer) -> IoResult<()> {
+  #[allow(unused_must_use)]
+  pub fn write_request(&self, w: &mut Writer) -> IoResult<()> {
     
       // Command
-      write!(w, "{} ", self.command);
+      write!(w, "{}", self.command);
 
       w.write_str("\n");
 
@@ -67,30 +174,20 @@ impl fmt::Show for Frame {
 
 
 fn main() {
-  let mut stream = TcpStream::connect("localhost", 61613).unwrap();
+  let stream = TcpStream::connect("localhost", 61613).unwrap();
+  // REQUEST
   let mut frame = Frame::new("CONNECT".to_string());
-  frame.setHeader("accept-version".to_string(), "1.1".to_string());
-  frame.setHeader("host".to_string(), "localhost".to_string());
-  frame.setBody("Hello from Rust".to_string());
+  frame.set_header("accept-version".to_string(), "1.1".to_string());
+  frame.set_header("host".to_string(), "localhost".to_string());
+  frame.set_body("Hello from Rust".to_string());
   let mut writer = stream.clone();
-  frame.writeRequest(&mut writer);
+  let _ = frame.write_request(&mut writer);
   println!("Frame: {}", frame);
-  let mut line = std::string::String::new();
-  loop {
-    match stream.read_byte() {
-      Ok(0x00)                    => {
-        break;
-      }
-      Ok(c)                       => {
-        line.push_char(c as char); 
-      }
-      /*Ok(c)                       => {*/
-      /*    fail!("malformat: reads={:?} next={:?}", line, c);*/
-      /*}*/
-      Err(_)                      => {}
-    };
-  };
-  let s = line.as_slice();
-  println!("{}", s);
-  drop(stream); // close the connection
+  drop(writer);
+  // RESPONSE
+  let res = Response::with_stream(&stream);
+  println!("Success! Command: {}", res.command);
+  println!("Success! Headers: {}", res.headers);
+  drop(res.stream); // close the connection
 }
+
